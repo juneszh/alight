830e425aabc65b2e46cce5d19fbafb58e69e34d5
juneszh <juneszh@gmail.com>
2023-09-13 18:30:18 +0800

    Change email address.

--- src/RouteUtility.php
+++ src/RouteUtility.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of the Alight package.
  *
- * (c) June So <alight@juneszh.com>
+ * (c) June So <june@alight.cc>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
