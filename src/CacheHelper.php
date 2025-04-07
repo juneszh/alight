ac5e176b25878979f3d686405014dead2f77039c
juneszh <juneszh@gmail.com>
2025-04-07 15:53:52 +0800

    fix: CacheHelper::get() return type

--- src/CacheHelper.php
+++ src/CacheHelper.php
@@ -55,7 +55,7 @@ class CacheHelper
                         return $return;
                     });
                 } else {
-                    $return = $cache->delete($key[0]);
+                    $cache->delete($key[0]);
                 }
             }
         }
